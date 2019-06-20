bede4fd21dacf1189eabe36af05ea40b453252a9
abellinii <wadeabel12@hotmail.com>
2019-06-20 06:22:27 -0700

    manual build

---
+++ build/precache-manifest.e86b291484195535cd8965cba960a2be.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "55723299cb1a0e36296d5a9ffbf7fdae",
+    "url": "./static/media/chef.55723299.jpg"
+  },
+  {
+    "revision": "39062bd97e62fce1bdfd",
+    "url": "./static/js/main.2568942b.chunk.js"
+  },
+  {
+    "revision": "e6ea10976c65e41aac007c011a97cbd4",
+    "url": "./static/media/tweet.e6ea1097.svg"
+  },
+  {
+    "revision": "02d010d17bb6df6b6ce8c1bbe713a7b6",
+    "url": "./static/media/meal.02d010d1.jpg"
+  },
+  {
+    "revision": "f0f7363394be236aaac3feeb8bdde3a1",
+    "url": "./static/media/meal.f0f73633.gif"
+  },
+  {
+    "revision": "a197f0f63309e3bd3deaac71211259e3",
+    "url": "./static/media/bottom.a197f0f6.jpg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ea2a5f631e25edb2f883d29c208d813d",
+    "url": "./static/media/color.ea2a5f63.jpg"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "./static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "64c441887a3df2f3c614db7ff056b090",
+    "url": "./static/media/knifeandfork.64c44188.png"
+  },
+  {
+    "revision": "ed9b2709ccde8e9fd28c",
+    "url": "./static/js/2.f6477c20.chunk.js"
+  },
+  {
+    "revision": "ed9b2709ccde8e9fd28c",
+    "url": "./static/css/2.6d3f189f.chunk.css"
+  },
+  {
+    "revision": "dfa2b3e9b3b3d1cdf1b6b154b5928822",
+    "url": "./index.html"
+  }
+];
